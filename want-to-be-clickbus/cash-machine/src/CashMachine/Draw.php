b8d1b3ed3fa10ad78c8b8f78c35fd15015bb61a7
Jack Makiyama <jackmakiyama@gmail.com>
2014-08-05 03:20:42 -0400

    Created Draw

---
+++ want-to-be-clickbus/cash-machine/src/CashMachine/Draw.php
@@ -0,0 +1,48 @@
+<?php
+namespace CashMachine;
+
+use CashMachine\Bills;
+use CashMachine\Exceptions\NoteUnavailableException;
+
+class Draw
+{
+	private $bills;
+
+	public function __construct(Bills $bills)
+	{
+		$this->bills = $bills;
+	}
+
+	public function draw($value = null)
+	{
+		if (!$value) {
+			return null;
+		}
+
+		$bills       = $this->bills->getBills();
+		$smallerBill = $this->bills->getSmallerBill();
+		
+		$i = 0;
+		while ($value) {
+
+			if ($value < 0) {
+				throw new \InvalidArgumentException("Invalid value");
+				
+			}
+			
+			if($value < $smallerBill)
+				throw new NoteUnavailableException("Unavailable value");
+
+			if($value >= $bills[$i]){
+				$value -= $bills[$i];
+				$reserveBills[] = $bills[$i];
+			}
+
+			if($value < $bills[$i]){
+				$i++;
+			}
+		}
+
+		return $reserveBills;
+	}
+}
